f5195df81486dc3ab82a5765369e3d177563f59f
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-06-09 13:29:26 +0700

    merge front-end

--- src/AvaloniaApplication3/AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
+++ src/AvaloniaApplication3/AvaloniaApplication3/obj/Debug/net8.0/AvaloniaApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2a31abcdea4e32edecc9039f18014c4c6c1279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef1d2bfc9c128546b97b2004d646d6504425c2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
