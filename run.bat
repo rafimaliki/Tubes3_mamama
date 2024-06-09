4b27d7b07e81b2da1a906e542fe5566b3bdf8f0b
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-06-09 18:30:38 +0700

    modify batch file

--- run.bat
+++ run.bat
@@ -2,4 +2,5 @@ cd src\AvaloniaApplication3\AvaloniaApplication3
 dotnet build
 cd ..\..\..
 cd src\AvaloniaApplication3\AvaloniaApplication3\bin\Debug\net8.0\
+echo AvaloniaApplication3 >> run.bat
 AvaloniaApplication3
